3d4f0ab0e4d37a8b6946d282ccac37245867bed3
Larry Schoeneman <lschoeneman@gmail.com>
2019-07-17 13:46:23 -0500

    Completed Part 4

--- GraphQLClient/Program.cs
+++ GraphQLClient/Program.cs
@@ -1,23 +1,21 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using GraphQL.Common.Request;
 using GraphQLClient.Entities;
+using Newtonsoft.Json;
 
 namespace GraphQLClient
 {
-    class Program
+    internal class Program
     {
-        private const string  GraphQlServerUrl = "https://localhost:44326/graphQL";
-        static async Task Main(string[] args)
-        {
-            //var customers = await SimpleWebClientDemo();
-            var customers2 = await SimpleGraphQLClientDemo();
+        private const string GraphQlServerUrl = "https://localhost:44326/graphQL";
 
+        private static async Task Main(string[] args)
+        {
+            var customers = await SimpleWebClientDemo();
+            var customer = await SimpleGraphQLClientDemo();
         }
 
         private static async Task<Customer> SimpleGraphQLClientDemo()
@@ -41,49 +39,28 @@ namespace GraphQLClient
                                 name
                                 id
                               }
-                            }"
+                            }",
+                Variables = new {portfolioId = 1, customerId = 2}
             };
-//Query = @"{ 
-//                               customers 
-//                                 { 
-//                                    id 
-//                                    name 
-//                                    portfolios 
-//                                    { 
-//                                        id 
-//                                        stocks 
-//                                        { 
-//                                            id 
-//                                            name 
-//                                        } 
-//                                    }  
-//                                } 
-//                            }"
-//                        };
 
-                //var temp = new {x = 1, y = 2};
-            customerRequest.Variables = new {  portfolioId=  1, customerId = 2};
-            
             var graphQLClient = new GraphQL.Client.GraphQLClient(GraphQlServerUrl);
             var graphQLResponse = await graphQLClient.PostAsync(customerRequest);
-            var customer = graphQLResponse.GetDataFieldAs<Customer>("customer"); 
-            var name = customer.Name;
+            var customer = graphQLResponse.GetDataFieldAs<Customer>("customer");
             return customer;
-
         }
 
         private static async Task<List<Customer>> SimpleWebClientDemo()
         {
-            HttpClient client = new HttpClient
+            var client = new HttpClient
             {
                 BaseAddress = new Uri(GraphQlServerUrl)
             };
 
-            var response = await client.GetAsync(@" ? query={ customers { id name portfolios { id stocks { id name } }  } }");
+            var response =
+                await client.GetAsync(@" ? query={ customers { id name portfolios { id stocks { id name } }  } }");
             var stringResult = await response.Content.ReadAsStringAsync();
             var root = JsonConvert.DeserializeObject<RootObject>(stringResult);
             return root.Data.Customers;
-
         }
     }
 }
